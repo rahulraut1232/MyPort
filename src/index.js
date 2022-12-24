22b6a3e8c4e106723888930037ff4972cfa97ae4
Rahul Raut <rahulraut1232@gmail.com>
2022-12-24 13:02:28 +0530

    added login page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from './component/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const Welcome=()=>
+{ 
+        return <h1>Hello Nikhil!</h1> 
+} 
 root.render(
   <React.StrictMode>
-    <App />
+    <Welcome />
+    <App/>
   </React.StrictMode>
 );
 
